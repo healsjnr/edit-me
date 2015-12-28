a8532161f5cde9fb14768e7c016eba73ae44ae70
healsjnr <healsjnr@gmail.com>
2015-12-28 12:45:03 +1000

    Implement post back for s3 create

--- app/assets/javascripts/components/DocumentVersionForm.jsx
+++ app/assets/javascripts/components/DocumentVersionForm.jsx
@@ -7,30 +7,15 @@ var DocumentVersionForm = React.createClass({
                 "s3_link": ''
             };
         },
-        handleChange: function (e) {
-            var name = e.target.name;
-            var obj = {};
-            obj["" + name] = e.target.value;
-            this.setState(obj);
-        },
         handleCallback: function (data) {
             this.props.handleNewDocumentVersion(data);
             this.setState(this.getInitialState);
         },
-        handleSubmit: function (e) {
-            var requestData = JSON.stringify(this.state)
-            e.preventDefault();
-            var xhr = $.ajax({
-                url: '/document_versions',
-                type: 'POST',
-                data: requestData,
-                contentType: 'application/json',
-                accept: 'application/json',
-                dataType: 'json',
-                success: this.handleCallback
-            })
-        },
-        createDocument: function(requestData) {
+        handleFinished: function(signResult, file) {
+            this.setState({'s3_link': signResult.uploadedFileName});
+            var requestData = JSON.stringify(this.state);
+            console.log("request data: ");
+            console.log(requestData);
             $.ajax({
                 url: '/document_versions',
                 type: 'POST',
@@ -41,34 +26,21 @@ var DocumentVersionForm = React.createClass({
                 success: this.handleCallback
             })
         },
-        handleFinished: function(signResult, file) {
-            console.log("Finished called.");
-            console.log("File: " + file.name);
-            console.log(signResult);
-        },
-        valid: function () {
-            return this.state.s3_link;
-        },
         render: function () {
             return (
-                <div>
-                    <form className="form-inline" onSubmit={this.handleSubmit}>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="S3 Linke" name="s3_link"
-                                   value={this.state.s3_link} onChange={this.handleChange}/>
-                        </div>
-                        <button type="submit" className="btn btn-primary" disabled={!this.valid}>
-                            New Document
-                        </button>
-                    </form>
-                    <ReactS3Uploader
+                <div className="row">
+                    <div className="col-md-2">
+                      <div>Add new version</div>
+                    </div>
+                    <div className="col-md-2"><ReactS3Uploader
                         signingUrl="/s3/uploadUrl"
                         accept="*/*"
                         onProgress={this.onUploadProgress}
                         onError={this.onUploadError}
                         onFinish={this.handleFinished}
                         uploadRequestHeaders={{ 'x-amz-acl': 'public-read' }}
-                        contentDisposition="inline" />
+                        contentDisposition="inline"/>
+                    </div>
                 </div>
             );
         }
